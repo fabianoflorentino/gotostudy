65facd8ef5d66b855aa8e67ecf9a2cbf1eb6ef16
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-04-13 14:18:00 -0300

    feat: Add GetUserByID function to retrieve a user by their UUID

--- services/user_services.go
+++ services/user_services.go
@@ -3,8 +3,13 @@ package services
 import (
 	"github.com/fabianoflorentino/gotostudy/models"
 	"github.com/fabianoflorentino/gotostudy/repositories"
+	"github.com/google/uuid"
 )
 
 func GetAllUsers() ([]models.User, error) {
 	return repositories.GetAllUsers()
+}
+
+func GetUserByID(id uuid.UUID) (models.User, error) {
+	return repositories.GetUserByID(id)
 }
