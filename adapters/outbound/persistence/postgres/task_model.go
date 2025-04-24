0b7247bcb7f49d3dfc7979983edc116440728e9a
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-04-24 11:04:38 -0300

    refactor: Enhance documentation in task_model.go and user_model.go for improved clarity and understanding

--- adapters/outbound/persistence/postgres/task_model.go
+++ adapters/outbound/persistence/postgres/task_model.go
@@ -1,3 +1,6 @@
+// Package postgres provides the implementation of data persistence for the application
+// using a PostgreSQL database. It defines the data models and their mappings to the
+// database schema, enabling seamless interaction with the database.
 package postgres
 
 import (
@@ -6,6 +9,11 @@ import (
 	"github.com/google/uuid"
 )
 
+// Task represents a task entity in the system. It includes details such as
+// the task's unique identifier (ID), title, description, completion status,
+// timestamps for creation and updates, and the ID of the user who owns the task.
+// The struct is designed to work with GORM for database persistence, with
+// annotations specifying primary key, default values, and constraints.
 type Task struct {
 	ID          uuid.UUID `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
 	Title       string    `gorm:"not null"`
--- adapters/outbound/persistence/postgres/user_model.go
+++ adapters/outbound/persistence/postgres/user_model.go
@@ -1,3 +1,7 @@
+// Package postgres provides the implementation of data persistence
+// for the application using a PostgreSQL database. It defines the
+// data models and their relationships, which are used by GORM to
+// interact with the database.
 package postgres
 
 import (
@@ -6,6 +10,16 @@ import (
 	"github.com/google/uuid"
 )
 
+// User represents a user entity in the system.
+// It contains fields for storing user-specific information such as
+// a unique identifier (ID), username, email, and timestamps for
+// creation and last update. The ID is automatically generated as a UUID.
+// The Username and Email fields are unique and cannot be null.
+// The CreatedAt and UpdatedAt fields are automatically managed by GORM
+// to track when the user was created and last updated, respectively.
+// The Tasks field establishes a one-to-many relationship with the Task entity,
+// where each user can have multiple tasks. Changes to the user will cascade
+// to associated tasks on update or delete operations.
 type User struct {
 	ID        uuid.UUID `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
 	Username  string    `gorm:"unique;not null"`
