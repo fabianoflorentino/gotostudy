a115fcd60691f3b9e38fd89bc2064eb62e01b03a
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-04-24 10:53:26 -0300

    fix: Update error handling in New function to use log.Printf instead of log.Fatalf and correct AutoMigrate target

--- internal/app/container.go
+++ internal/app/container.go
@@ -3,8 +3,8 @@ package app
 import (
 	"log"
 
-	"github.com/fabianoflorentino/gotostudy/adapters/inbound/http/controllers"
 	"github.com/fabianoflorentino/gotostudy/adapters/outbound/persistence/postgres"
+	"github.com/fabianoflorentino/gotostudy/core/domain"
 	"github.com/fabianoflorentino/gotostudy/core/services"
 	"github.com/fabianoflorentino/gotostudy/database"
 	"gorm.io/gorm"
@@ -17,15 +17,15 @@ type AppContainer struct {
 
 func New() *AppContainer {
 	if err := database.InitDB(); err != nil {
-		log.Fatalf("failed to initialize database: %v", err)
+		log.Printf("failed to initialize database: %v", err)
 	}
 
 	dbConn := database.DB
 	repo := postgres.NewPostgresUserRepository(dbConn)
 	service := services.NewUserService(repo)
 
-	if err := dbConn.AutoMigrate(&controllers.UserController{}); err != nil {
-		log.Fatalf("failed to migrate database: %v", err)
+	if err := dbConn.AutoMigrate(&domain.User{}); err != nil {
+		log.Printf("failed to migrate database: %v", err)
 	}
 
 	return &AppContainer{
