b6348c59bca77058040c9a1b76b55788b4974911
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-04-23 21:58:53 -0300

    feat: Add HealthController with HealthCheck endpoint

---
+++ adapters/inbound/http/controllers/health_controller.go
@@ -0,0 +1,17 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type HealthController struct{}
+
+func NewHealthController() *HealthController {
+	return &HealthController{}
+}
+
+func (h *HealthController) HealthCheck(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "OK"})
+}
