796089081a9648ec33ab530ed178f480b0d3dcaf
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-04-25 12:24:13 -0300

    docs: Enhance comments in task.go and user.go for improved clarity on domain models

--- core/domain/task.go
+++ core/domain/task.go
@@ -1,3 +1,7 @@
+// Package domain contains the core business entities and domain models
+// used in the application. These models represent the fundamental
+// concepts and rules of the problem domain, and they are designed to
+// be independent of any specific application layer or infrastructure.
 package domain
 
 import (
@@ -6,6 +10,10 @@ import (
 	"github.com/google/uuid"
 )
 
+// Task represents a to-do item or activity that a user can create and manage.
+// It includes fields for a unique identifier (ID), title, description,
+// completion status (Completed), timestamps for creation and updates
+// (CreatedAt and UpdatedAt), and the ID of the user who owns the task (UserID).
 type Task struct {
 	ID          uuid.UUID
 	Title       string
--- core/domain/user.go
+++ core/domain/user.go
@@ -1,3 +1,6 @@
+// Package domain contains the core business entities and domain logic
+// for the application. It defines the primary data structures and
+// relationships that represent the problem domain.
 package domain
 
 import (
@@ -6,6 +9,10 @@ import (
 	"github.com/google/uuid"
 )
 
+// User represents a user entity in the system.
+// It contains the user's unique identifier, username, email, and timestamps
+// for when the user was created and last updated. Additionally, it includes
+// a list of tasks associated with the user.
 type User struct {
 	ID        uuid.UUID
 	Username  string
