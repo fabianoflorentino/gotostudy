ed7533efe75c84cf4445957b9af8c33a8aa9a3da
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-06-14 17:02:19 -0300

    feat: add custom error messages for user-related operations

---
+++ core/errors.go
@@ -0,0 +1,9 @@
+package core
+
+import "errors"
+
+var (
+	ErrUserNotFound       = errors.New("user not found")
+	ErrUserAlreadyExists  = errors.New("user already exists")
+	ErrInvalidUpdateField = errors.New("invalid update fields")
+)
