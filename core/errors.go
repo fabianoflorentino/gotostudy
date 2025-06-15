aa473e7603385a2a44a1421004a0b56b4a804b1d
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-06-15 14:05:08 -0300

    fix: add missing error for email already exists in core errors

--- core/errors.go
+++ core/errors.go
@@ -5,5 +5,6 @@ import "errors"
 var (
 	ErrUserNotFound       = errors.New("user not found")
 	ErrUserAlreadyExists  = errors.New("user already exists")
+	ErrEmailAlreadyExists = errors.New("email already exists")
 	ErrInvalidUpdateField = errors.New("invalid update fields")
 )
