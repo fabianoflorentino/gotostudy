89051120323550ea8f027fe5034b7b7ebab32a5f
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-06-21 10:20:04 -0300

    feat: add ErrNoTasksFound error for user task retrieval

--- core/errors.go
+++ core/errors.go
@@ -7,4 +7,5 @@ var (
 	ErrUserAlreadyExists  = errors.New("user already exists")
 	ErrEmailAlreadyExists = errors.New("email already exists")
 	ErrInvalidUpdateField = errors.New("invalid update fields")
+	ErrNoTasksFound       = errors.New("no tasks found for user")
 )
