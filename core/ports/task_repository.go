5870d2334d00228aa9ac4fd183de1732f55d66b6
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-06-24 08:56:53 -0300

    refactor: update Save method in TaskRepository to include userID parameter

--- core/ports/task_repository.go
+++ core/ports/task_repository.go
@@ -13,7 +13,7 @@ import (
 // TaskRepository defines the interface for interacting with task data storage.
 // It provides methods to find, save, update, and delete tasks.
 type TaskRepository interface {
-	Save(ctx context.Context, task *domain.Task) error
+	Save(ctx context.Context, userID uuid.UUID, task *domain.Task) error
 	FindUserTasks(ctx context.Context, userID uuid.UUID) ([]*domain.Task, error)
 	FindTaskByID(ctx context.Context, taskID uuid.UUID) (*domain.Task, error)
 	Update(ctx context.Context, id uuid.UUID, task *domain.Task) error
