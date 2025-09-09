5bdb463554603a3be6d0768a69491d46f1039038
Fabiano Santos Florentino <fabianoratm@gmail.com>
2025-09-09 20:15:18 -0300

    feat: add validation functions for email format and task title

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -38,6 +38,8 @@ func IsEmailInUse(ports ports.UserRepository, ctx context.Context, email string,
 	return true, nil
 }
 
+// IsEmailValid validates the format of the given email string.
+// It returns core.ErrInvalidEmail if the email format is invalid, or nil if it is valid.
 func IsEmailValid(email string) error {
 	// Use a simple regex to validate the email format
 	const emailRegex = `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
@@ -45,4 +47,17 @@ func IsEmailValid(email string) error {
 		return core.ErrInvalidEmail
 	}
 	return nil
+}
+
+// IsTaskTitleValid checks if the provided task title is valid according to predefined rules.
+// It returns true if the title is valid, false otherwise.
+func IsTaskTitleValid(title string) bool {
+	// Task title must be at least 3 characters long and not just whitespace
+	if len(title) < 3 {
+		return false
+	}
+	if regexp.MustCompile(`^\s*$`).MatchString(title) {
+		return false
+	}
+	return true
 }
